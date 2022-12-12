f71f37b0555681ff768a31b85d90163a7c73885c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-12 12:47:47 +0100

    new hover effect using js

--- src/main.js
+++ src/main.js
@@ -166,3 +166,14 @@ anchorToCertificates.onclick = (e) => {
     window.location.href = target;
   }, 1500);
 };
+
+document.querySelector(".swiper-wrapper").onmousemove = e => {
+  for(const card of document.getElementsByClassName("swiper-slide")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
