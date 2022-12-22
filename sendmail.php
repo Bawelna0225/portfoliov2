8d20e0688eca534ff4c6be2284188bf24aee0950
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-12-22 21:24:32 +0100

    fixed bug with subject message

--- sendmail.php
+++ sendmail.php
@@ -2,13 +2,13 @@
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
-    $subject = $_POST['subject'];
+    $subjectInput = $_POST['subject'];
     $messageInput = $_POST['message'];
     $date = date('Y-m-d');
 
     $receiver = "pawelczarnecki0225@gmail.com";
     $subject = "From: $firstName $lastName <$email>";
-    $body = "Name: $firstName $lastName \nEmail: $email\nSubject: $subject\nMessage: $messageInput";
+    $body = "Name: $firstName $lastName \nEmail: $email\nSubject: $subjectInput\nMessage: $messageInput";
     $sender = "From: $email";
     if(mail($receiver, $subject, $body, $sender)){
         echo "Your message has been sent";
@@ -16,7 +16,7 @@
         echo "Sorry, failed to send your message!";
     }
 
-    $conn = new mysqli('localhost', '', '', '');
+    $conn = new mysqli('', '', '', '');
     $query = "INSERT INTO maile VALUES(null, '$firstName $lastName', '$email', '$subject', '$messageInput', '$date')";
     mysqli_query($conn, $query);
     mysqli_close($conn)
